4d004e15696cbff7a77d1c3a881f29d92c29ee2f
ajsalkt3399 <ajsalkt@gmail.com>
2023-07-22 16:00:44 +0530

    first page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import "./App.css";
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
+      <RowPost/>
     </div>
   )
 }
